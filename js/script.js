ece2b9ae3452f21ed125f70c5d3db0fa29324e28
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-04-25 13:08:28 +0200

    stampo mail

--- js/script.js
+++ js/script.js
@@ -5,13 +5,23 @@ createApp({
 
   data(){
     return{
-
+      mail: '',
+      url:'https://flynn.boolean.careers/exercises/api/random/mail?items=10',
     }
 
   },
+  methods:  {
+    getApi(){
+      axios.get(this.url)
+      .then((result) =>{
+        this.mail = result.data.response
+      })
+
+    }
+  },
 
   mounted(){
-    
+    this.getApi();
   }
 
 
